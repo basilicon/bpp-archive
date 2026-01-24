52eebafa78f13b03dc87262cc99afe7d895c0983
Ethan Curtis <arcademedes@gmail.com>
2026-01-24 14:39:57 -0500

    Add game ID to random panel API response and update template for game link

--- app.py
+++ app.py
@@ -116,6 +116,7 @@ def api_panel_random():
         "id": panel.id,
         "content_url": panel.content_url,
         "book_id": panel.book_id,
+        "game_id": panel.book.game_id,
         "author": panel.author_alias.name,
         "prompt": panel.book.pages[0].content_text, # The first text page
         "sequence": panel.sequence
